4f6c628aa4f5a2b710966d50a1f5cabc84938d3c
kanoonhazan <hazanhzn200@gmail.com>
2026-01-15 22:49:22 +0530

    Add Vercel Analytics 0.1

--- App.tsx
+++ App.tsx
@@ -30,55 +30,10 @@ const AppContent: React.FC = () => {
     };
   }, []);
 
-  const schemaMarkup = [
-    {
-      "@context": "https://schema.org",
-      "@type": "Organization",
-      "name": "NAXIT",
-      "url": "https://www.naxitofficial.de",
-      "logo": "https://www.naxitofficial.de/logo.png",
-      "sameAs": [
-        "https://www.linkedin.com/company/naxitofficial/",
-        "https://www.facebook.com/naxitofficial",
-        "https://whatsapp.com/channel/0029VaCY8ulCHDyowtpSzx30"
-      ]
-    },
-    {
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      "name": "NAXIT",
-      "image": "https://www.naxitofficial.de/logo.png",
-      "url": "https://www.naxitofficial.de",
-      "telephone": "+94758089209",
-      "address": {
-        "@type": "PostalAddress",
-        "addressLocality": "Mannar",
-        "addressRegion": "Northern Province",
-        "addressCountry": "LK"
-      },
-      "areaServed": {
-        "@type": "Country",
-        "name": "Sri Lanka"
-      },
-      "priceRange": "$$"
-    },
-    {
-      "@context": "https://schema.org",
-      "@type": "ContactPoint",
-      "contactType": "customer support",
-      "telephone": "+94758089209",
-      "areaServed": "LK",
-      "availableLanguage": ["English"]
-    }
-  ];
-
   return (
     <div className="relative selection:bg-naxit-cyan selection:text-naxit-charcoal bg-naxit-charcoal min-h-screen">
       <Helmet>
         <html lang="en-LK" />
-        <script type="application/ld+json">
-          {JSON.stringify(schemaMarkup)}
-        </script>
       </Helmet>
       <NeuralBackground />
       <Routes>
